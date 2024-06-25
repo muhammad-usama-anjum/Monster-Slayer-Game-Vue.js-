d74ccf84bd465b36db920d2efae1a65c03199fde
muhammad-usama-anjum <usamaanjum67@gmail.com>
2024-06-25 14:56:03 +0500

    Added new button of start new game, resolve the issue of bars not getting empty after game finished, Activated the Surrender button and deactivated the button after game over

--- App.js
+++ App.js
@@ -15,9 +15,17 @@ const app = Vue.createApp({
     },
     computed:{
         monsterBarStyles(){
+            if(this.monsterHealth<0)
+                {
+                    return{width: '0%'}
+                }
             return{width: this.monsterHealth + '%'};
         },
         playerBarStyles(){
+            if(this.playerHealth<0)
+                {
+                    return{width: '0%'}
+                }
             return{width: this.playerHealth + '%'};
         },
         mayUseSpecialAttack(){
@@ -76,6 +84,15 @@ const app = Vue.createApp({
                 }
 
                 this.attackPlayer();
+        },
+        startGame(){
+            this.playerHealth=100;
+            this.monsterHealth=100;
+            this.currentRound=0;
+            this.winner=null;
+        },
+        surrender(){
+            this.winner='monster';
         }
 
     }
