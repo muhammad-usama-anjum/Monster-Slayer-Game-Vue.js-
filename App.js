254e7de23b14ebdac2a590938d32ccaf8ef44711
muhammad-usama-anjum <usamaanjum67@gmail.com>
2024-06-25 15:53:03 +0500

    Added the Battle log functionality

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ const app = Vue.createApp({
         playerHealth: 100,
         monsterHealth: 100,
         currentRound: 0,
-        winner: null
+        winner: null,
+        logMessages:[]
        }
 
     },
@@ -60,16 +61,19 @@ const app = Vue.createApp({
             this.currentRound++;
             const attackValue=getRandomValue(5,12);
             this.monsterHealth -=attackValue;
+            this.addLogMessage('player', 'attack', attackValue);
             this.attackPlayer();
         },
         attackPlayer(){
             const attackValue=getRandomValue(8,15);
             this.playerHealth-=attackValue;
+            this.addLogMessage('monster', 'attack', attackValue);
         },
         specialAttackMonster(){
             this.currentRound++;
             const attackValue=getRandomValue(10,25);
             this.monsterHealth-=attackValue;
+            this.addLogMessage('player', 'special-attack', attackValue);
             this.attackPlayer();
         },
         healPlayer(){
@@ -83,16 +87,27 @@ const app = Vue.createApp({
                     this.playerHealth +=healValue;
                 }
 
+                this.addLogMessage('player', 'heal', healValue);
+
                 this.attackPlayer();
         },
         startGame(){
             this.playerHealth=100;
             this.monsterHealth=100;
             this.currentRound=0;
+            this.logMessages=[];
             this.winner=null;
         },
         surrender(){
             this.winner='monster';
+        },
+        addLogMessage(who, what, value){
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            })
+
         }
 
     }
