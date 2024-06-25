d01b58f57fb44feb5812b2369acea8f4d10911e4
muhammad-usama-anjum <usamaanjum67@gmail.com>
2024-06-25 14:15:11 +0500

    Activated the Heal button

--- App.js
+++ App.js
@@ -18,6 +18,9 @@ const app = Vue.createApp({
         },
         playerBarStyles(){
             return{width: this.playerHealth + '%'};
+        },
+        mayUseSpecialAttack(){
+            return this.currentRound % 3 !==0;
         }
 
     },
@@ -37,6 +40,19 @@ const app = Vue.createApp({
             const attackValue=getRandomValue(10,25);
             this.monsterHealth-=attackValue;
             this.attackPlayer();
+        },
+        healPlayer(){
+            this.currentRound++;
+            const healValue=getRandomValue(8,20);
+            if(this.playerHealth + healValue >100)
+                {
+                    this.playerHealth=100;
+                }
+                else{
+                    this.playerHealth +=healValue;
+                }
+
+                this.attackPlayer();
         }
 
     }
